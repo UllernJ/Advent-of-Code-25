f8d671b9c14c9ab7eb64c5b0cb0e60d83699e41b
UllernJ <ulrik.johannesen@gmail.com>
2025-12-13 20:49:49 +0100

    day6

---
+++ src/solutions/Day06.kt
@@ -0,0 +1,42 @@
+package solutions
+
+import utils.readInput
+
+private val input = readInput("Day06").map { it -> it.split(" ").filter { it.isNotEmpty() } }
+
+fun main() {
+    solvePart1()
+}
+
+fun solvePart1() {
+    val map = mutableMapOf<Int, MutableList<Int>>()
+    var sum = 0L
+    input.forEach { it ->
+        it.forEachIndexed { indexY, num ->
+            if (num.toIntOrNull() == null) {
+                sum += calculateList(map[indexY], num)
+            } else {
+                map.getOrPut(indexY) { mutableListOf() }.add(num.toInt())
+            }
+        }
+    }
+    println(sum)
+}
+
+private fun calculateList(list: MutableList<Int>?, operator: String): Long {
+    var sum = 0L
+    when (operator) {
+        "+" -> {
+            list?.forEach {
+                sum += it
+            }
+        }
+        "*" -> {
+            sum = 1
+            list?.forEach {
+                sum *= it
+            }
+        }
+    }
+    return sum
+}
